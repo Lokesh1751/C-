c02b8c7f8fa446d750a425433887e1d8150aff9e
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-18 09:57:38 +0530

    new commit

--- Practice/callbyreference.cpp
+++ Practice/callbyreference.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
-void swap(int &a,int &b){
-    int c=a;
-    a=b;
-    b=c;
+void swap(int *a, int *b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
 }
-int main(){
-    int a,b;
-    cin>>a>>b;
-    cout<<a<<" "<<b<<endl;
-    swap(a,b);
-    cout<<a<<" "<<b<<endl;
-
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a << " " << b << endl;
+    swap(&a, &b);
+    cout << a << " " << b << endl;
 }
