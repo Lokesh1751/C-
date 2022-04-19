f51538e411cd453661ce2cd973759d440d514fa3
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-19 10:21:49 +0530

    new commit

--- Practice/ComplexNumber.cpp
+++ Practice/ComplexNumber.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
-class Complexnumber{
-int real;
-int imaginery;
-public:
-void read(){
-    cout<<"Enter real part:"<<endl;
-    cin>>real;
-    cout<<"Enter Imaginery part"<<endl;
-    cin>>imaginery;
-}
-void display(){
-    if(imaginery>0 && imaginery>0){
-        cout<<real<<"+"<<imaginery<<"i";
+class Complexnumber
+{
+    int real;
+    int imaginery;
 
+public:
+    void read()
+    {
+        cout << "Enter real part:" << endl;
+        cin >> real;
+        cout << "Enter Imaginery part" << endl;
+        cin >> imaginery;
     }
-    
-}
+    void display()
+    {
+        if (imaginery > 0 && imaginery > 0)
+        {
+            cout << real << "+" << imaginery << "i";
+        }
+    }
 };
-int main(){
-    Complexnumber cn;
+int main()
+{
+    Complexnumber cn, cn1;
     cn.read();
     cn.display();
     return 0;
-
 }
