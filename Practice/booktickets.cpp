ec9d783f34281ea4cfa18278ca001ac09402f4a8
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-19 14:38:01 +0530

    new commit

---
+++ Practice/booktickets.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class booking
+{
+public:
+    static int count;
+
+public:
+    booking(int p)
+    {
+        count += p;
+    }
+};
+int main()
+{
+    booking b(10);
+
+    cout << b.count << endl;
+    booking b1(10);
+    
+    cout << b1.count << endl;
+}
