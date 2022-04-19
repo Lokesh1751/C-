f3d276d79d0be49e7b533de47910798fd54a2b64
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-19 11:25:56 +0530

    new commit

--- Practice/ComplexNumber.cpp
+++ Practice/ComplexNumber.cpp
@@ -2,26 +2,28 @@
 using namespace std;
 class Complexnumber
 {
-    public:
+public:
     int real;
     int imaginery;
-// Complexnumber(int x,int y){
-//    this->real=x;
-//    this->imaginery=y;
-    
+    // Complexnumber(int x,int y){
+    //    this->real=x;
+    //    this->imaginery=y;
 
+    // }
+    Complexnumber()
+    {
+        this->real = 0;
+        this->imaginery = 0;
+    }
 
-// }
-Complexnumber(){
-    this->real=0;
-    this->imaginery=0;
-}
 public:
     void read()
     {
-        cout << "Enter real part:" << endl;
+        cout << "Enter real part:"
+             << " ";
         cin >> real;
-        cout << "Enter Imaginery part" << endl;
+        cout << "Enter Imaginery part"
+             << " ";
         cin >> imaginery;
     }
     void display()
@@ -31,14 +33,25 @@ public:
             cout << real << "+" << imaginery << "i";
         }
     }
+    Complexnumber addition(Complexnumber cn1)
+    {
+        Complexnumber c3;
+        c3.real = real + cn1.real;
+        c3.imaginery = imaginery + cn1.imaginery;
+        return c3;
+    }
 };
 int main()
 {
-    Complexnumber cn;
+    Complexnumber cn,cn1,c3;
+    cn1.read();
+    cn1.display();
+    cout << endl;
     cn.read();
     cn.display();
+    cout<<endl;
+    c3 = cn.addition(cn1);
+    c3.display();
     return 0;
-
-       
- 
+    
 }
