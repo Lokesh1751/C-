de1c148a290985b2f9bca155e1687f4885d09c83
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-22 17:56:49 +0530

    new commit

---
+++ Queue/createqueue.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    queue<int> q;
+
+    q.push(11);
+    cout<<"front of queue is:"<<" "<<q.front()<<endl;
+
+    q.push(15);
+
+    q.push(13);
+
+    cout << "Size of queue is:"
+         << " " << q.size() << endl;
+         q.pop();
+         cout<<"front of queue is:"<<" "<<q.front()<<endl;
+      cout << "Size of queue is:"
+         << " " << q.size() << endl;
+        if(q.empty()){
+            cout<<"Queue is empty"<<endl;
+        }
+        else{
+            cout<<"Queue is not empty"<<endl;
+        }
+
+}
