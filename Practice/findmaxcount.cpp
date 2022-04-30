073bc27d2ce8b73d672e1febbef24a53a8d3aa79
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-30 18:02:07 +0530

    max count

---
+++ Practice/findmaxcount.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+#include <limits.h>
+using namespace std;
+int main()
+{
+    int arr[8] = {1,2,3,4,5,6,1,10};
+    int count;
+    int maxx = INT_MIN;
+    int ans=0;
+    for (int i = 0; i < 8; i++)
+    {
+        count = 0;
+        for (int j = i; j < 8; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+                maxx = max(maxx, count);
+                ans=arr[i];
+
+            }
+        }
+    }
+    cout << ans << endl;
+}
