12ef9562cc8af4fb7313eaa24c73804b5f54e273
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-05-03 11:20:41 +0530

    new commit

---
+++ Practice/implicitfunction.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=100;
+    char y='a';
+    char t;
+    float z;
+    // x=100+y;
+    // cout<<x<<endl;
+
+    // implicit function
+    t=x;
+    cout<<t<<endl;
+    // explicit function
+    z=(float)x;
+    cout<<z<<endl;
+    z=297.3;
+    // explicit function
+    x=(float)z;
+    cout<<x<<endl;
+
+
+
+}
