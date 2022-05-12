aec71b876da57349fcfe519d606c78b15dc57806
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-05-12 18:01:53 +0530

    insert at right and left node

--- Binary Search Tree/InsertNode.cpp
+++ Binary Search Tree/InsertNode.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
 using namespace std;
-class Node{
+class Node
+{
 public:
-int data;
-Node* right;
-Node* left;
-Node(int d){
-    this->data=d;
-    this->left=NULL;
-    this->right=NULL;
-}
+    int data;
+    Node *right;
+    Node *left;
+    Node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
 };
-Node* insertIntoBST(Node* &root,int d){
-    if(root==NULL){
-        root=new Node(d);
+Node *insertIntoBST(Node *&root, int d)
+{
+    if (root == NULL)
+    {
+        root = new Node(d);
         return root;
     }
-
-
-
-}
-void takeInput(Node* &root){
-    int data;
-    cin>>data;
-    while(data!=-1){
-        insertIntoBST(root,data);
-        cin>>data;
+    if (d > root->data)
+    {
+        // right part me insert krna h
+        root->right= insertIntoBST(root->right, d);
     }
+    if (d < root->data)
+    {
+        // left part me insert krna hai
+       root->left= insertIntoBST(root->left, d);
+    }
+    return root;
 
 }
-int main(){
-    Node* root=NULL;
-    cout<<"Enter data to create BST:"<<endl;
+void takeInput(Node *&root)
+{
+    int data;
+    cin >> data;
+    while (data != -1)
+    {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+}
+int main()
+{
+    Node *root = NULL;
+    cout << "Enter data to create BST:" << endl;
     takeInput(root);
-
 }
