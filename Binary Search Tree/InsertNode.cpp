fda4c6d5b70d18c3489f379e5a32e05d69e0a5e7
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-05-12 17:57:11 +0530

    root node created

--- Binary Search Tree/InsertNode.cpp
+++ Binary Search Tree/InsertNode.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 using namespace std;
+class Node{
+public:
+int data;
+Node* right;
+Node* left;
+Node(int d){
+    this->data=d;
+    this->left=NULL;
+    this->right=NULL;
+}
+};
+Node* insertIntoBST(Node* &root,int d){
+    if(root==NULL){
+        root=new Node(d);
+        return root;
+    }
+
+
+
+}
+void takeInput(Node* &root){
+    int data;
+    cin>>data;
+    while(data!=-1){
+        insertIntoBST(root,data);
+        cin>>data;
+    }
+
+}
 int main(){
-    
+    Node* root=NULL;
+    cout<<"Enter data to create BST:"<<endl;
+    takeInput(root);
+
 }
