2dcb8f95ffb146bf7cfab4de024c78b8f5cb65b1
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-05-17 21:14:43 +0530

    deletion from heap

--- Heap/insertion.cpp
+++ Heap/insertion.cpp
@@ -1,41 +1,85 @@
 #include <iostream>
 using namespace std;
-class Heap{
+class Heap
+{
 public:
-int arr[100];
-int size;
-Heap(){
-    arr[0]=-1;
-    size=0;
-}
-void insert(int val){
-    size=size+1;
-    int index=size;
-    arr[index]=val;
-    while(index>1){
-        int parent= index/2;
-        if(arr[parent] < arr[index]){
-            swap(arr[parent],arr[index]);
-            index=parent;
-        }
-        else{
-            return;
+    int arr[100];
+    int size;
+    Heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+    void insert(int val)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else
+            {
+                return;
+            }
         }
     }
+    void print()
+    {
+        for (int i = 1; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    void deletefromheap ()
+    {
+        if (size == 0)
+        {
+            cout << "Nothing to delete" << endl;
+            return;
+        }
+        // put last element to last node
+        arr[1]=arr[size];
+        // last node delete
+        size--;
+        // take root node to its correct position
+        int i=1;
+        while(i<size){
+            int leftindex=2*i;
+            int rightindex=2*i + 1;
+            if(leftindex < size && arr[i] < arr[leftindex]){
+                swap(arr[i],arr[leftindex]);
+                i=leftindex;
+            }
+            else if(rightindex < size && arr[i] < arr[rightindex]){
+                swap(arr[i],arr[rightindex]);
+                i=rightindex;
+            }
+            else{
+                return;
+            }
+        }
 
-}
-void print(){
-    for(int i=1;i<size;i++){
-        cout<<arr[i]<<" ";
-    } cout<<endl;
-}
+
+    }
 };
-int main(){
-Heap h;
-h.insert(50);
-h.insert(55);
-h.insert(53);
-h.insert(52);
-h.print();
-return 0;
+int main()
+{
+    Heap h;
+    h.insert(50);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
+    h.print();
+    // root node delete
+    h.deletefromheap();
+    h.print();
+    return 0;
 }
