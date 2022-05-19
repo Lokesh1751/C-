dee9c7794c8c3d96d1ea13a47b5fe345d7c2d7d4
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-05-19 18:12:34 +0530

    changes done

--- Practice/kthlargest.cpp
+++ Practice/kthlargest.cpp
@@ -1,20 +1,26 @@
 // kth largest sum subarray
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main()
 {
 
     int arr[5] = {1, 2, 3, 4, 5};
+    int n=5;
     vector<int> ans;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < 5; j++)
+        for (int j = i + 1; j < n; j++)
         {
             ans.push_back(arr[i]);
             ans.push_back(arr[i] + arr[j]);
         }
     }
-    cout<<"Kth Largest Element is :"<<" ";
-    cout<<ans[2]<<endl;
+    sort(ans.begin(),ans.end());
+    int k;
+    cout<<"K is:"<<" ";
+    cin>>k;
+     cout<<"Kth Largest Element is :"<<" ";
+    cout<<ans[n-k-1]<<endl;
 }
