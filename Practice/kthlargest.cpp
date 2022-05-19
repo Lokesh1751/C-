6dfa683e6d23e07759897257e7ebb73ea04562d3
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-05-19 18:00:28 +0530

    kth largest subarray sum

---
+++ Practice/kthlargest.cpp
@@ -0,0 +1,19 @@
+// kth largest sum subarray
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    vector<int> ans;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i + 1; j < 5; j++)
+        {
+            ans.push_back(arr[i] + arr[j]);
+        }
+    }
+    cout<<"Kth Largest Element is :"<<" ";
+    cout<<ans[2]<<endl;
+}
